1788c2b908f4fde8d21d5de4f5e17066f8cbc00c
Gaikwad556 <gamingonearth556@gmail.com>
2025-09-22 23:21:23 +0530

    billing Address checkout test

---
+++ Selenium-Practice/src/CheckoutFunction_DemoWebShop/BillingAddressCheckoutPOM.java
@@ -0,0 +1,215 @@
+package CheckoutFunction_DemoWebShop;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+public class BillingAddressCheckoutPOM {
+	private WebDriver driver;
+
+	public BillingAddressCheckoutPOM(WebDriver d) {
+		super();
+		this.driver = d;
+		PageFactory.initElements(driver, this);
+	}
+	
+//	locators
+//	login
+	@FindBy(xpath = "//a[@class='ico-login']")
+	private WebElement login;
+	
+	@FindBy(id = "Email")
+	private WebElement loginEmail;
+	
+	@FindBy(id = "Password")
+	private WebElement loginPassword;
+	
+	@FindBy(xpath = "//input[@value='Log in']")
+	private WebElement loginButton;
+	
+	private By loginBooks = By.xpath("//a[@href='/books']");
+	
+
+	@FindBy(css = "input.product-box-add-to-cart-button")
+	private List<WebElement> loginCartButtons;
+	
+
+	@FindBy(css = ".ajax-loading-block-window")
+	private WebElement loginLoder;
+	
+
+	@FindBy(xpath = "//a[@href='/cart' and @class='ico-cart']")
+	private WebElement loginShoppingCartLink;
+	
+
+	@FindBy(id = "termsofservice")
+	private WebElement loginDialogBox;
+
+	@FindBy(id = "checkout")
+	private WebElement loginCheckoutButton;
+	
+	
+	
+//	Billing addresss locator
+	@FindBy(id = "BillingNewAddress_FirstName")
+	private WebElement firstNameField;
+	
+	@FindBy(id = "BillingNewAddress_LastName")
+	private WebElement lastNameField;
+	
+	@FindBy(id = "BillingNewAddress_Email")
+	private WebElement emailField;
+	
+	@FindBy(id = "BillingNewAddress_Company")
+	private WebElement companyField;
+	
+	@FindBy(id = "BillingNewAddress_CountryId")
+	private WebElement countryDropDown;
+	
+	@FindBy(id = "BillingNewAddress_City")
+	private WebElement cityField;
+	
+	@FindBy(id = "BillingNewAddress_Address1")
+	private WebElement address1Field;
+	
+	@FindBy(id = "BillingNewAddress_Address2")
+	private WebElement address2Field;
+	
+	@FindBy(id = "BillingNewAddress_ZipPostalCode")
+	private WebElement zipCode;
+	
+	@FindBy(id = "BillingNewAddress_PhoneNumber")
+	private WebElement phoneNumber;
+	
+	@FindBy(id = "BillingNewAddress_FaxNumber")
+	private WebElement faxNumber;
+	
+	@FindBy(css = "#billing-buttons-container>input")
+	private WebElement continueButton;
+	
+//	Assert loctors
+	@FindBy(xpath = "//span[@data-valmsg-for='BillingNewAddress.FirstName']")
+	private WebElement firstNameAssert;
+
+//	user login
+	public void login() {
+		login.click();		
+		loginEmail.sendKeys("gaik@gamil.com");
+		loginPassword.sendKeys("Gaikwad@27");
+		loginButton.click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		bookClick();
+	}
+	
+	private void bookClick() {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		WebElement book = wait.until(ExpectedConditions.elementToBeClickable(loginBooks));
+		book.click();
+	}
+	
+
+//	cart button
+	public void clickAddtoCartButton() throws InterruptedException {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(7));
+		wait.until(ExpectedConditions.visibilityOf(loginCartButtons.get(0)));
+		for (WebElement button : loginCartButtons) {
+			wait.until(ExpectedConditions.invisibilityOf(loginLoder));
+			button.click();
+		}
+	}
+	
+
+//	shopping cart link
+	public void cartLinkClick() {
+		Actions action = new Actions(driver);
+		action.moveToElement(loginShoppingCartLink).click().perform();;
+	}
+	
+	public void checkoutButtonClick() {
+		loginDialogBox.click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.visibilityOf(loginCheckoutButton)).click();
+	}
+	
+	
+//	Billing Address interaction methods
+	public void firstNameInteraction(String firstName) {
+		firstNameField.sendKeys(firstName);
+	}
+	
+	public void lastNameInteraction(String lastName) {
+		lastNameField.sendKeys(lastName);
+	}
+	
+	public void emailInteraction(String email) {
+		emailField.sendKeys(email);
+	}
+	
+	public void companyFieldInteraction(String company) {
+		companyField.sendKeys(company);
+	}
+	
+	public void countryDropDownInteraction(String countryName) {
+		Select country = new Select(countryDropDown);
+		country.selectByVisibleText(countryName);;
+	}
+	
+	public void cityInteraction(String cityName) {
+		cityField.sendKeys(cityName);
+	}
+	
+	public void Address1Interaction(String addressOne) {
+		address1Field.sendKeys(addressOne);
+	}
+	
+	public void Address2Interaction(String addressTwo) {
+		address2Field.sendKeys(addressTwo);
+	}
+	
+	public void zipCodeInteraction(String zip) {
+		zipCode.sendKeys(zip);
+	}
+	
+	public void phoneNumberInteraction(String phone) {
+		phoneNumber.sendKeys(phone);
+	}
+	
+	public void faxNumberInteraction(String fax) {
+		faxNumber.sendKeys(fax);
+	}
+	
+	public void continueClick() {
+		continueButton.click();
+	}
+	
+	public void fieldClear() {
+		firstNameField.clear();
+		lastNameField.clear();
+		emailField.clear();
+		companyField.clear();
+		cityField.clear();
+		address1Field.clear();
+		address2Field.clear();
+		zipCode.clear();
+		phoneNumber.clear();
+		faxNumber.clear();
+	}
+	
+	
+//	Assert Check
+	public void firstNameAssertCheck(String firstName) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		WebElement first = wait.until(ExpectedConditions.visibilityOf(firstNameAssert));
+		Assert.assertEquals(first.getText(), firstName);
+	}
+}
---
+++ Selenium-Practice/src/CheckoutFunction_DemoWebShop/billingAddressCheckoutScript.java
@@ -0,0 +1,63 @@
+package CheckoutFunction_DemoWebShop;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class billingAddressCheckoutScript {
+	WebDriver driver;
+	BillingAddressCheckoutPOM checkpom;
+
+	@BeforeTest
+	public void setUp() throws InterruptedException {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demowebshop.tricentis.com/");
+		checkpom = new BillingAddressCheckoutPOM(driver);
+		checkpom.login();
+		checkpom.clickAddtoCartButton();
+		checkpom.cartLinkClick();
+		checkpom.checkoutButtonClick();
+	}
+	
+	@BeforeMethod
+	public void fieldClear() {
+		checkpom.fieldClear();
+	}
+	
+	@Test
+	public void firstNameEmpty() {
+//		field values
+		String firstName = "";
+		String lastName = "ssg";
+		String email = "gaik@gamil.com";
+		String company = "";
+		String countryValue = "India";
+		String city = "Mumbai";
+		String address1 = "Navi Mumbai";
+		String address2 = "";
+		String zipCode = "215200";
+		String phone = "7458932105";
+		String fax = "";
+//	Assert value
+		String firstNameAssertCheck = "First name is required.";
+//		Interaction
+		checkpom.firstNameInteraction(firstName);
+		checkpom.lastNameInteraction(lastName);
+		checkpom.emailInteraction(email);
+		checkpom.companyFieldInteraction(company);
+		checkpom.countryDropDownInteraction(countryValue);
+		checkpom.cityInteraction(city);
+		checkpom.Address1Interaction(address1);
+		checkpom.Address2Interaction(address2);
+		checkpom.zipCodeInteraction(zipCode);
+		checkpom.phoneNumberInteraction(phone);
+		checkpom.faxNumberInteraction(fax);
+//		button click
+		checkpom.continueClick();
+//		assert check
+		checkpom.firstNameAssertCheck(firstNameAssertCheck);
+	}
+}
